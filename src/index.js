37dbad26231692399a402eccf1cf7da39f8ef225
60162260_KimHyunKyung <tbgusrud12@gmail.com>
2020-10-26 15:01:26 +0900

    [koa-router settings]

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+
 const app = new Koa();
+const router = new Router();
 
-app.use((ctx,next)=>{
-    console.log(ctx.url);
-    console.log(1);
-    //next();
+
+//라우터 설정
+router.get('/',ctx=>{
+    ctx.body = '홈';
 });
-app.use((ctx, next) => {
-    console,log(2);
-    next();
+router.get('/about',ctx=>{
+    ctx.body = '소개';
 });
 
-app.use(ctx=>{
-    ctx.body ='hello world';
-})
 
+app.use(router.routes()).use(router.allowedMethods());
 
-
-app.listen(4000,()=>{
-    console.log("Listening to port 4000");
-})
+app.listen(4000, () => {
+    console.log('Listening to port 4000');
+});
