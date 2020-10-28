e4a58bcda106f2023e9a2e31c1c2d2d46c8366f1
60162260_KimHyunKyung <tbgusrud12@gmail.com>
2020-10-29 08:59:32 +0900

    setting to esm

--- src/index.js
+++ src/index.js
@@ -1,40 +1,4 @@
-require('dotenv').config();
+/* eslint-diavble no-global-assign */
 
-const Koa = require('koa');
-const Router = require('koa-router');
-
-const app = new Koa();
-const router = new Router();
-
-const mongoose = require('mongoose');
-
-mongoose.Promise = global.Promise;
-
-mongoose.connect(process.env.MONGO_URI,{
-    useMongoClient: true
-}).then(
-    (response) => {
-        console.log('Successfully connected to mongodb')
-    }
-).catch(e=>{
-    console.error(e);
-});
-
-const port = process.env.PORT || 4000;
-
-
-
-//라우터 설정
-router.get('/',ctx=>{
-    ctx.body = '홈';
-});
-router.get('/about',ctx=>{
-    ctx.body = '소개';
-});
-
-
-app.use(router.routes()).use(router.allowedMethods());
-
-app.listen(port, () => {
-    console.log('Listening to port ' + port);
-});
+require = require('esm')(module /*, options*/);
+ module.exports = require('./main.js');
