84e152af37138c617dee47801f4292c5cfe2df6a
60162260_KimHyunKyung <tbgusrud12@gmail.com>
2020-10-29 09:53:51 +0900

    yarn add bcrypt

--- src/models/user.js
+++ src/models/user.js
@@ -1,8 +1,11 @@
+// 사용자 정보 스키마
 import mongoose, { Schema } from 'mongoose';
 
 const UserSchema = new Schema({
-  username: String,
-  hashedPassword: String,
+  username: String, // 사용자 이름
+  userCel: String, // 사용자 전화번호
+  userId: String, // 사용자 아이디
+  hashedPassword: String, // 사용자 비밀번호
 });
 
 const User = mongoose.model('User', UserSchema);
