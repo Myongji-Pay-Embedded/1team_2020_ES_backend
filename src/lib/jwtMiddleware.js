5e866c5ad84b707dd0b19906855f008c1a421f11
60162260_KimHyunKyung <tbgusrud12@gmail.com>
2020-11-02 21:09:34 +0900

    async-await

--- src/lib/jwtMiddleware.js
+++ src/lib/jwtMiddleware.js
@@ -2,7 +2,7 @@
 import jwt from 'jsonwebtoken';
 import User from '../models/user';
 
-const jwtMiddleware = (ctx, next) => {
+const jwtMiddleware = async (ctx, next) => {
   const token = ctx.cookies.get('access_token');
   if (!token) return next(); // 토큰이 없음
   try {
@@ -13,14 +13,14 @@ const jwtMiddleware = (ctx, next) => {
     };
 
     // 토큰의 남은 유효기간이 3.5이 미만이면 재발급
-    const now = Math.floor(Date.now()/1000);
-    if(decoded.exp - now<(60*60*24*3.5)){
-        const user = await User.findById(decoded._id);
-        const token = user.generateToken();
-        ctx.cookies.set('access_token', token, {
-            maxAge: 1000 * 60 * 60 * 24 * 7, // 7일
-            httpOnly: true,
-        });
+    const now = Math.floor(Date.now() / 1000);
+    if (decoded.exp - now < 60 * 60 * 24 * 3.5) {
+      const user = await User.findById(decoded._id);
+      const token = user.generateToken();
+      ctx.cookies.set('access_token', token, {
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7일
+        httpOnly: true,
+      });
     }
     return next();
   } catch (e) {
