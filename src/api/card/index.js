cbceac65f27141c5b7f0ab3bf31c31c7329e7e6a
60162260_KimHyunKyung <tbgusrud12@gmail.com>
2020-11-17 17:43:53 +0900

    Membership Card

--- src/api/card/index.js
+++ src/api/card/index.js
@@ -1,19 +1,17 @@
-// 5개의 라우터
+// // 5개의 라우터
 
-import Router from 'koa-router';
-import * as cardsCtrl from './cards.ctrl';
-import checkLoggedIn from '../../lib/checkLoggedIn';
+// import Router from 'koa-router';
+// import * as cardsCtrl from './cards.ctrl';
+// import checkLoggedIn from '../../lib/checkLoggedIn';
 
-const cards = new Router();
+// const cards = new Router();
 
-cards.get('/', checkLoggedIn, cardsCtrl.list);
-cards.post('/', checkLoggedIn, cardsCtrl.add);
+// cards.get('/', checkLoggedIn, cardsCtrl.list);
+// cards.post('/', checkLoggedIn, cardsCtrl.add);
 
-const card = new Router(); // /api/cards/:id
-card.get('/:id', checkLoggedIn, cardsCtrl.read);
-card.delete('/:id', checkLoggedIn, cardsCtrl.remove);
-card.patch('/:id', checkLoggedIn, cardsCtrl.update);
+// const card = new Router(); // /api/cards/:id
+// card.get('/', checkLoggedIn, cardsCtrl.read);
+// card.delete('/', checkLoggedIn, cardsCtrl.remove);
+// card.patch('/', checkLoggedIn, cardsCtrl.update);
 
-cards.use('/:id', cardsCtrl.checkLoggedIn, card.routes());
-
-export default cards;
+// cards.use('/:id', cardsCtrl.getCardById, card.routes());
--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,14 @@
 import Router from 'koa-router';
-import cards from './card';
+//import cards from './card';
 import auth from './auth';
 import account from './account';
-import membership from './membership';
+import memberships from './membership';
 
 const api = new Router();
 //api.use('/cards', cards.routes());
 api.use('/auth', auth.routes());
 api.use('/account', account.routes());
-api.use('/memberships', membership.route());
+api.use('/memberships', memberships.routes());
 
 //라우터 보내기
 export default api;
